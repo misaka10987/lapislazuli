b6f952a7176062c4007f7c29487c1a49ea083f49
misaka10987 <misaka10987@outlook.com>
2024-10-28 19:13:20 +0800

    add automated script to check c++17 compatibility

---
+++ check.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+main() {
+    g++ -std=c++23 -Wall -fsyntax-only $1
+    set +e
+    g++ -std=c++17 -o /dev/null $1 >/dev/null 2>/dev/null
+    local succ=$?
+    set -e
+    if [ $succ -ne 0 ]; then
+        echo "failure"
+        exit $succ
+    fi
+    echo "success"
+}
+
+main $@
