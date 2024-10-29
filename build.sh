16e4a188838a833ce3ff4b07f325f72db2c07fa1
misaka10987 <misaka10987@outlook.com>
2024-10-29 10:11:24 +0800

    update buildscript

--- build.sh
+++ build.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 set -e
-mkdir -p ./target
-gcc -fpreprocessed -dD -E ./src/lib.cc > ./target/lapislazuli
-vim -c ":%s/^# .*//g|:%s/\(\"[^\"]*\)\@<!\s\{2,}/ /g|:%j|:%s/\(.\)\zs#\([^#]\)/\r\1#\2/g|:%s/namespace/\rnamespace/g|:%s/ #/#/|:wq" ./target/lapislazuli
+
+build() {
+    mkdir -p ./target
+    gcc -fpreprocessed -dD -E ./src/lib.cc >./target/lapislazuli
+    vim -c ":%s/^# .*//g|:%s/\(\"[^\"]*\)\@<!\s\{2,}/ /g|:%j|:%s/\(.\)\zs#\([^#]\)/\r\1#\2/g|:%s/namespace/\rnamespace/g|:%s/ #/#/|:wq" ./target/lapislazuli
+}
+
+subs() {
+    sed -e '/#define lapislazuli/ {' -e 'r ./target/lapislazuli' -e 'd' -e '}' -i $1
+}
+
+main() {
+    if [ $# -eq 0 ]; then
+        build
+    else
+        subs $1
+    fi
+}
+
+main $@
