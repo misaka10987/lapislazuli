a3570060ef53ae3d3931e81f984d8b56d6185ca8
misaka10987 <misaka10987@outlook.com>
2024-10-29 09:04:33 +0800

    add soft link in project root, update buildscript

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 mkdir -p ./target
-gcc -fpreprocessed -dD -E ./src/lib.cc > ./target/lapislazuli.cc
-vim -c ":%s/\(\"[^\"]*\)\@<!\s\{2,}/ /g|:%j|:%s/\(.\)\zs#\([^#]\)/\r\1#\2/g|:%s/namespace/\rnamespace/g|:wq" ./target/lapislazuli.cc
+gcc -fpreprocessed -dD -E ./src/lib.cc > ./target/lapislazuli
+vim -c ":%s/^# .*//g|:%s/\(\"[^\"]*\)\@<!\s\{2,}/ /g|:%j|:%s/\(.\)\zs#\([^#]\)/\r\1#\2/g|:%s/namespace/\rnamespace/g|:wq" ./target/lapislazuli
