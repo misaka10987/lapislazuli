ba42dc2381d604dcc5b4c2aee44fdd09e3c58694
misaka10987 <misaka10987@outlook.com>
2024-10-29 09:14:19 +0800

    update buildscript

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 set -e
 mkdir -p ./target
 gcc -fpreprocessed -dD -E ./src/lib.cc > ./target/lapislazuli
-vim -c ":%s/^# .*//g|:%s/\(\"[^\"]*\)\@<!\s\{2,}/ /g|:%j|:%s/\(.\)\zs#\([^#]\)/\r\1#\2/g|:%s/namespace/\rnamespace/g|:wq" ./target/lapislazuli
+vim -c ":%s/^# .*//g|:%s/\(\"[^\"]*\)\@<!\s\{2,}/ /g|:%j|:%s/\(.\)\zs#\([^#]\)/\r\1#\2/g|:%s/namespace/\rnamespace/g|:%s/ #/#/|:wq" ./target/lapislazuli
